488e2ce3b3c24459ef7ea406b9205855c3019b7e
joansenent <joansenent@hotmail.com>
2014-01-12 11:44:31 +0000

    fixing the bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -11,9 +11,17 @@ describe Bike do
 		# a method "broke?" that should return false
 		expect(bike).not_to be_broken
 	end
+
 	it "should be able to break" do
 		bike = Bike.new
 		bike.break
 		expect(bike).to be_broken
+	end
+
+	it "should be able to get fixed" do
+		bike = Bike.new
+		bike.break
+		bike.fix
+		expect(bike).not_to be_broken
 	end
 end
